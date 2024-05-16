58d01069f1b276f54cc8a96a62e2f9b594f2bcb2
bkobei <115329058+bkobei@users.noreply.github.com>
2024-05-16 13:41:52 -0700

    Add files via upload

--- game_storing_class.py
+++ game_storing_class.py
@@ -1,13 +1,19 @@
 class game_storing:
-    def __init__(self, name, genre_list, developer, publisher):
+    def __init__(self, name, genre_list, developer, publisher, prices, sale_percent="0%"):
         self.name = name
         self.genres = genre_list
+        self.prices = prices
         if type(genre_list) is not list:
             print("arg2 is invalid. Double check inputs.")
+            raise TypeError
+        
+        if type(prices) is not list:
+            print("arg2 is invalid type. Double check inputs.")
             raise TypeError
 
         self.developer = developer
         self.publisher = publisher
+        self.sale_percent = sale_percent
 
     def get_name(self):
         return self.name
@@ -21,27 +27,6 @@ class game_storing:
     def get_publisher(self):
         return self.publisher
         
-    def is_discounted(self):
-        if self.prices[1] < self.prices[0]:
-            return True
-        else:
-            return False
-        
-class game_storing_2:
-    def __init__(self, name, prices, position):
-        self.name = name
-        self.prices = prices
-        self.position = position
-
-        if type(prices) is not list:
-            print("arg2 is invalid type. Double check inputs.")
-            raise TypeError
-    def get_position(self):
-        return self.position
-        
-    def get_name(self):
-        return self.name
-    
     def get_prices(self):
         return self.prices
     
@@ -54,3 +39,26 @@ class game_storing_2:
         else:
             print("There is no discount")
             return self.get_original_price()
+        
+    def get_sale_percent(self):
+        return self.sale_percent
+        
+    def is_discounted(self):
+        if len(self.prices > 1):
+            return True
+        else:
+            return False
+        
+class game_storing_2:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    def get_position(self):
+        return self.position
+        
+    def get_name(self):
+        return self.name
+    
+    
+    
