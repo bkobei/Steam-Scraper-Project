22ccf070743e04d87ae887645e0392f5d8ae5631
bkobei <115329058+bkobei@users.noreply.github.com>
2024-05-16 00:31:51 -0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2022 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""An example of showing geographic data."""
+
+import os
+
+import altair as alt
+import numpy as np
+import pandas as pd
+import pydeck as pdk
+import streamlit as st
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
+
+
+# LOAD DATA ONCE
+@st.cache_resource
+def load_data():
+    path = os.pathh.norm(os.path.join(os.cwd(), 'analysis.json'))
+    with open(path, 'r') as file:
+        file_data = json.load(file)
+
+    return file_data
+
+
+# FILTER DATA BY HOUR
+@st.cache_data
+def histdata(df, hr):
+    filtered = data[
+        (df["date/time"].dt.hour >= hr) & (df["date/time"].dt.hour < (hr + 1))
+    ]
+
+    hist = np.histogram(filtered["date/time"].dt.minute, bins=60, range=(0, 60))[0]
+
+    return pd.DataFrame({"minute": range(60), "pickups": hist})
+
+
+# STREAMLIT APP LAYOUT
+data = load_data()
+
+# LAYING OUT THE TOP SECTION OF THE APP
+# row1_1, row1_2 = st.columns((2, 3))
+
+# IF THE SLIDER CHANGES, UPDATE THE QUERY PARAM
+def update_query_params():
+    hour_selected = st.session_state["pickup_hour"]
+    st.experimental_set_query_params(pickup_hour=hour_selected)
+
+
+with row1_1:
+    st.title("NYC Uber Ridesharing Data")
+    hour_selected = st.slider(
+        "Select hour of pickup", 0, 23, key="pickup_hour", on_change=update_query_params
+    )
+
+
+with row1_2:
+    st.write(
+        """
+    ##
+    Examining how Uber pickups vary over time in New York City's and at its major regional airports.
+    By sliding the slider on the left you can view different slices of time and explore different transportation trends.
+    """
+    )
+
+# LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
+row2_1, row2_2, row2_3, row2_4 = st.columns((2, 1, 1, 1))
+
+# SETTING THE ZOOM LOCATIONS FOR THE AIRPORTS
+la_guardia = [40.7900, -73.8700]
+jfk = [40.6650, -73.7821]
+newark = [40.7090, -74.1805]
+zoom_level = 12
+midpoint = mpoint(data["lat"], data["lon"])
+
+with row2_1:
+    st.write(
+        f"""**All New York City from {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
+    )
+    map(filterdata(data, hour_selected), midpoint[0], midpoint[1], 11)
+
+with row2_2:
+    st.write("**La Guardia Airport**")
+    map(filterdata(data, hour_selected), la_guardia[0], la_guardia[1], zoom_level)
+
+with row2_3:
+    st.write("**JFK Airport**")
+    map(filterdata(data, hour_selected), jfk[0], jfk[1], zoom_level)
+
+with row2_4:
+    st.write("**Newark Airport**")
+    map(filterdata(data, hour_selected), newark[0], newark[1], zoom_level)
+
+# CALCULATING DATA FOR THE HISTOGRAM
+chart_data = histdata(data, hour_selected)
+
+# LAYING OUT THE HISTOGRAM SECTION
+st.write(
+    f"""**Breakdown of rides per minute between {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
+)
+
+st.altair_chart(
+    alt.Chart(chart_data)
+    .mark_area(
+        interpolate="step-after",
+    )
+    .encode(
+        x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
+        y=alt.Y("pickups:Q"),
+        tooltip=["minute", "pickups"],
+    )
+    .configure_mark(opacity=0.2, color="red"),
+    use_container_width=True,
+)
